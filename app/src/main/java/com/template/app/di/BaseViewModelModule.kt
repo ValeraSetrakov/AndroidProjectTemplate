35acc281ad78b679d91a21767a7b5c6e23a79088
valerijsetrakov <valerasetrakov@gmail.com>
2021-01-17 17:43:45 +0300

    1) Extructed module for providing view model factory to external lib

--- app/src/main/java/com/template/app/di/BaseViewModelModule.kt
+++ app/src/main/java/com/template/app/di/BaseViewModelModule.kt
@@ -1,23 +1,16 @@
 package com.template.app.di
 
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
 import com.template.app.features.splash.SplashActivityViewModel
 import com.template.app.features.splash.fragment.SplashFragmentViewModel
-import com.template.library.client.di.viewmodel.ViewModelFactory
 import com.template.library.client.di.viewmodel.ViewModelKey
+import com.template.library.client.di.viewmodel.ViewModelModule
 import dagger.Binds
 import dagger.Module
 import dagger.multibindings.IntoMap
-import dagger.multibindings.Multibinds
 
-@Module
+@Module(includes = [ViewModelModule::class])
 abstract class BaseViewModelModule {
-    @Binds
-    abstract fun bindViewModelFactory(viewModelFactory: ViewModelFactory): ViewModelProvider.Factory
-
-    @Multibinds
-    abstract fun emptyInjectorsMap(): Map<Class<out ViewModel>, ViewModel>
 
     @Binds
     @IntoMap
---
+++ library/src/main/java/com/template/library/client/di/viewmodel/ViewModelModule.kt
@@ -0,0 +1,19 @@
+package com.template.library.client.di.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import dagger.Binds
+import dagger.Module
+import dagger.multibindings.Multibinds
+
+/**
+ * Base module for providing [ViewModelProvider].[Factory]
+ */
+@Module
+abstract class ViewModelModule {
+    @Binds
+    abstract fun bindViewModelFactory(viewModelFactory: ViewModelFactory): ViewModelProvider.Factory
+
+    @Multibinds
+    abstract fun emptyInjectorsMap(): Map<Class<out ViewModel>, ViewModel>
+}
